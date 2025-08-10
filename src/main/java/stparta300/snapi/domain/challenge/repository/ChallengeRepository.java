0af08492de4b7e30ac764e6f74c0207f9b61976c
JuHyeonAh <jha08130930@gmail.com>
2025-08-11 01:49:38 +0900

    JPA 세팅

--- src/main/java/stparta300/snapi/domain/challenge/repository/ChallengeRepository.java
+++ src/main/java/stparta300/snapi/domain/challenge/repository/ChallengeRepository.java
@@ -1,3 +1,6 @@
 package stparta300.snapi.domain.challenge.repository;
 
-import org.springframework.data.jpa.repos
+import org.springframework.data.jpa.repository.JpaRepository;
+import stparta300.snapi.domain.challenge.entity.Challenge;
+
+public interface ChallengeRepository extends JpaRepository<Challenge, Long> { }
--- src/main/java/stparta300/snapi/domain/mission/repository/MissionRepository.java
+++ src/main/java/stparta300/snapi/domain/mission/repository/MissionRepository.java
@@ -1,3 +1,6 @@
 package stparta300.snapi.domain.mission.repository;
 
-import org.springframework.data.jpa.rep
+import org.springframework.data.jpa.repository.JpaRepository;
+import stparta300.snapi.domain.mission.entity.Mission;
+
+public interface MissionRepository extends JpaRepository<Mission, Long> { }
